7d1933c608fa6f9ce94c1cc6b87e214f0aa8b881
Redshadow31 <charbonnier.damien88@gmail.com>
2025-08-31 01:42:18 +0200

    Delete netlify/functions/logout.js

--- netlify/functions/logout.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 302,
-  headers: {
-    // cookie expiré
-    "Set-Cookie": "nf_session=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; Secure",
-    "Location": "/"
-  }
-});
